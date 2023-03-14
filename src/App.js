197ffa503dd5ead8965d73bc2866b9bdda35e6a0
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-03-14 15:17:24 +0600

    tailwind and daisy ui set

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import { AuthContext } from "./Contexts/UserContext/UserContext";
 function App() {
   const { googleSignIn } = useContext(AuthContext);
 
-  const handle = () => {
-    googleSignIn()
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err.code, err.message));
-  };
-
   return (
     <>
-      <button onClick={handle}>Sign in</button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
