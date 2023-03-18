68ec3fce5b50a3e8db1ebe0492530b8b0e35b4fc
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-03-18 21:47:05 +0600

    theme set

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -4,13 +4,18 @@ import { AuthContext } from "../../../Contexts/UserContext/UserContext";
 
 const Header = () => {
   const { logOut, user } = useContext(AuthContext);
-  /* const [theme, setTheme] = useState("");
+  // const selectedTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
   useEffect(() => {
     return document.querySelector("html").setAttribute("data-theme", theme);
   }, [theme]);
-   */
-  const clickHandle = (theme) => {
-    document.querySelector("html").setAttribute("data-theme", theme);
+
+  const clickHandler = (themeName) => {
+    // document.querySelector("html").dataset.theme = themeName;
+    localStorage.setItem("theme", themeName);
+
+    setTheme(themeName);
   };
 
   return (
@@ -67,75 +72,47 @@ const Header = () => {
           FAQ
         </NavLink>
       </div>
-      <div className="flex-1 justify-end">
-        <div className="dropdown dropdown-end">
-          {/* <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </label> */}
-          <div
-            tabIndex={0}
-            className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
+
+      {/*  */}
+
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            {user?.photoURL ? (
+              <img src={user?.photoURL} />
+            ) : (
+              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            )}
           </div>
-        </div>
+        </label>
 
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li>
+            <span>toggle</span>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+          {user?.email || user?.uid ? (
+            <li>
+              <Link onClick={logOut}>Logout</Link>
+            </li>
+          ) : null}
+        </ul>
+      </div>
+
+      <div className="flex-1 justify-end">
+        {/*  */}
         <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              {user?.photoURL ? (
-                <img src={user?.photoURL} />
-              ) : (
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              )}
-            </div>
-          </label>
-
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <span>toggle</span>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            {user?.email || user?.uid ? (
-              <li>
-                <Link onClick={logOut}>Logout</Link>
-              </li>
-            ) : null}
-          </ul>
-
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
               {user?.photoURL ? (
@@ -154,42 +131,41 @@ const Header = () => {
           >
             <li
               onClick={() => {
-                clickHandle("cupcake");
+                clickHandler("cupcake");
               }}
             >
               <span>Cupcake</span>
             </li>
             <li
               onClick={() => {
-                clickHandle("cyberpunk");
+                clickHandler("cyberpunk");
               }}
             >
               <span>CyberPunk</span>
             </li>
             <li
               onClick={() => {
-                clickHandle("dark");
+                clickHandler("dark");
               }}
             >
               <span>Dark</span>
             </li>
             <li
               onClick={() => {
-                clickHandle("night");
+                clickHandler("night");
               }}
             >
               <span>Night</span>
             </li>
             <li
               onClick={() => {
-                clickHandle("night");
+                clickHandler("night");
               }}
             >
               <span>Select Default Theme</span>
             </li>
           </ul>
           {/* WITH ONCLICK FUNCTION */}
-
           {/* CHANGE THIS SECTION OPTIONS TO THEMES */}
         </div>
       </div>
