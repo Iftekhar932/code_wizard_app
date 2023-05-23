6cada73703234ebbdefd02cb0b320d3a075a52e2
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-05-23 10:53:36 +0600

    mongodb test

--- src/Pages/FAQ/FAQ.jsx
+++ src/Pages/FAQ/FAQ.jsx
@@ -4,14 +4,14 @@ import { useLoaderData } from "react-router-dom";
 
 const FAQ = () => {
   const loadedUsers = useLoaderData();
-  const [users, setUsers] = useState(loadedUsers);
+  const [users, setUsers] = useState({});
+  const [displayUsers, setDisplayUsers] = useState(loadedUsers);
 
   const handleBlur = (e) => {
     const field = e.target.name;
     const value = e.target.value;
     const newUser = { ...users };
     newUser[field] = value;
-    console.log(newUser);
     setUsers(newUser);
   };
 
@@ -25,9 +25,48 @@ const FAQ = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.acknowledged) {
-          console.log(data);
-          return alert("user created");
+          alert("user created");
         }
+      });
+  };
+  const updateHandler = (e, userInfo) => {
+    e.preventDefault();
+    fetch(`http://localhost:5000/users/${userInfo.id}`, {
+      method: " PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(users),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert("user created");
+        }
+      });
+  };
+
+  const deleteHandler = (userInfo) => {
+    const confirm = window.confirm("Are you sure you want to delete?");
+    if (confirm) {
+      fetch(`http://localhost:5000/users/${userInfo._id}`, { method: "DELETE" })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            const remainingUsers = displayUsers.filter(
+              (user) => user._id != userInfo._id
+            );
+            setDisplayUsers(remainingUsers);
+            return alert("user deleted");
+          }
+        });
+    }
+  };
+
+  const deleteAllHandler = () => {
+    fetch("http://localhost:5000/users", { method: "DELETE" })
+      .then((res) => res.json())
+      .then((data) => {
+        setDisplayUsers({});
+        console.log(data);
       });
   };
 
@@ -47,10 +86,29 @@ const FAQ = () => {
         </button>
       </form>
       <ul>
-        {users.map((usr) => {
-          return <li key={usr._id}>{usr.userName}</li>;
+        {displayUsers?.map((usr) => {
+          return (
+            <li key={usr._id}>
+              {usr.userName}{" "}
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteHandler(usr)}
+              >
+                Delete
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => updateHandler(usr)}
+              >
+                Update
+              </button>
+            </li>
+          );
         })}
       </ul>
+      <button className="btn btn-danger" onClick={deleteAllHandler}>
+        Delete All
+      </button>
     </div>
   );
 };
